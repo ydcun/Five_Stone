5f4ea16878eddf3628c9c2504707860207ee68f9
ydcun <ydcun@hotmail.com>
2014-06-23 03:11:17 +0800

    五子棋
    Signed-off-by: ydcun <ydcun@hotmail.com>

---
+++ src/game/gobang/MainFrame.java
@@ -0,0 +1,27 @@
+package game.gobang;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+/**
+ * 五子棋
+ * @WangJB
+ * @version 19:46 13/03/17
+ */
+public class MainFrame extends JFrame{
+    public static void main(String[] args) {
+    	MainPanel panel = new MainPanel();
+		MainFrame frame = new MainFrame("五子棋");
+		frame.setSize(680,680);
+	    panel.setBackground(Color.GRAY);
+		frame.add(panel,BorderLayout.CENTER);
+		panel.addMouseListener(panel);
+		frame.setVisible(true);
+	}
+    public MainFrame(){
+    	super();
+    }
+    public MainFrame(String str){
+    	super(str);
+    }
+}
